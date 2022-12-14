2a45d158efc85a51dd71b6f20ed207d0d7bd7a4c
Appi72020 <abdoulraouf72020@gmail.com>
2022-12-14 01:06:50 +0100

    DoTween
    -Mise en place du FrameWork DoTween

---
+++ Virtual-world-UI/Assets/_Script/Teewn.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teewn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOScale(7, 5);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
